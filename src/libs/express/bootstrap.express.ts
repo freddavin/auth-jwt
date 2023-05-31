ed9607d1d43c0e1c651a8fbb6b9d7bb69292d9aa
Frederico Ferreira <fredericodavin@gmail.com>
2023-05-30 23:06:38 -0300

    refac: change auth token

--- src/libs/express/bootstrap.express.ts
+++ src/libs/express/bootstrap.express.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { logger } from '../winston';
-import { userRouter } from '../../routes';
+import { authRouter, userRouter } from '../../routes';
 
 export const bootstrapExpress = (port: number) => {
   const app = express();
 
   app.use(express.json());
   app.use('/users', userRouter);
+  app.use('/auth', authRouter);
 
   app.listen(port, () => {
     logger.info('🚀 [Express] Bootstrapped', { port });
