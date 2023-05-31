2709651009cebf31281b15c2c5c296975755fafa
Frederico Ferreira <fredericodavin@gmail.com>
2023-05-30 23:06:38 -0300

    refac: change auth token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,27 +1,7 @@
 import { Request, Response } from 'express';
-import { compare } from 'bcrypt';
-import { sign } from 'jsonwebtoken';
-import { User } from '../models/user';
-import config from 'config';
+import { authService } from '../services/auth.service';
 
-export const authController = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email, password });
-
-  if (!user) {
-    return res.status(400).send({ error: 'User not found!' });
-  }
-
-  const isValidPassword = await compare(user.password, password);
-
-  if (!isValidPassword) {
-    return res.status(400).send({ error: 'Password id not valid!' });
-  }
-
-  const secret = config.get<string>('SECRET_HASH');
-
-  const token = sign({ id: user.id }, secret, { expiresIn: '2h' });
-
-  return res.send({ id: user.id, token });
+export const authHandler = async (req: Request, res: Response) => {
+  const result = await authService(req.body);
+  return res.status(result.status).send(result.message);
 };
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -4,9 +4,13 @@ import {
   getUserHandler,
   createUserHandler,
 } from '../controllers/user.controller';
+import { authHandler } from '../controllers/auth.controller';
 
 export const userRouter = express.Router();
+export const authRouter = express.Router();
 
 userRouter.get('/', getUsersHandler);
 userRouter.get('/:id', getUserHandler);
 userRouter.post('/', createUserHandler);
+
+authRouter.post('/', authHandler);
