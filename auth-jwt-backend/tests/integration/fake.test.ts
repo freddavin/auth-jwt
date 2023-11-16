3568b5e59547140f571568596033a165281057cd
Frederico Ferreira <fredericodavin@gmail.com>
2023-11-16 11:19:40 -0300

    fix: integration first test

--- auth-jwt-backend/tests/integration/fake.test.ts
+++ auth-jwt-backend/tests/integration/fake.test.ts
@@ -1,35 +1,24 @@
-import request from 'supertest';
-// import { Types } from 'mongoose';
-import { expressApp } from '../libs/express';
-import { IUser } from '../../src/models/user';
-import { listUserById } from '../../src/services/users';
+import request from "supertest";
+import { expressApp } from "../libs/express";
+import { IUser } from "../../src/models/user";
+import { listUserById } from "../../src/services/users";
 
-describe('User Controller - Integration Tests', () => {
-  describe('Create User', () => {
-    // it('fake test', async () => {
-    //   const userId = new Types.ObjectId().toString();
-
-    //   await request(expressApp)
-    //     .get(`/users/${userId}`)
-    //     .expect(404, { message: 'User not found' });
-    // });
-
-    // it('fake test', async () => {
-    //   await request(expressApp).get(`/users`).expect(200);
-    // });
-
-    it('When a valid input request is sent. Then should be able to create an user', async () => {
+describe("Auth Controller - Integration Tests", () => {
+  describe("Register Handler", () => {
+    it("When a valid input request is sent. Then should be able to register an user", async () => {
       const user: IUser = {
-        name: 'test_name',
-        email: 'test@test.com',
-        password: 'test123',
+        name: "test_name",
+        email: "test@test.com",
+        password: "test123",
       };
 
-      const response = await request(expressApp).post(`/users`).send(user);
-      const { id: resultId } = response.body;
+      const response = await request(expressApp)
+        .post(`/auth/register`)
+        .send(user);
+      const { id } = response.body;
 
-      const expectedUser = await listUserById({ id: resultId });
-      expect(resultId).toEqual(expectedUser.id);
+      const expectedUser = await listUserById(id);
+      expect(id).toEqual(expectedUser.id);
     });
   });
 });
